9b2f6b801aef34f16944e551fe2931db202b43d4
shaochuan <shaochuan.wang@gmail.com>
2021-12-04 21:23:09 -0800

    Demonstrate if-else in the template

--- hello.py
+++ hello.py
@@ -9,3 +9,11 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.route('/user_no_name')
+def user_no_name():
+    return render_template('user.html')
+
+@app.route('/list/<num>')
+def list(num):
+    return render_template('list.html', num=int(num))
