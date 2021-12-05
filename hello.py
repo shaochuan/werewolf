2f3f90beccec5b18a7a962f39648d9b82af59d64
shaochuan <shaochuan.wang@gmail.com>
2021-12-04 21:49:02 -0800

    Simple form

--- hello.py
+++ hello.py
@@ -1,19 +1,34 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'randomo string'
+
+bootstrap = Bootstrap(app)
+
+class NameForm(FlaskForm):
+    name = StringField('Name:', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
